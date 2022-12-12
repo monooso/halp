e33194b6e4643648f3ed21ead1fee705a5f7e69e
Stephen Lewis <gpg@stephenlewis.me>
2022-12-12 11:33:35 +0000

    Require ex_machina for testing

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule Halp.MixProject do
       {:phoenix, "~> 1.7.0-rc.0", override: true},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
+      {:ex_machina, "~> 2.7.0", only: :test},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
